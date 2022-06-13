102c457d7eda5d856289a7d27621dc7593945b5b
Sumit Singh <sumitsingh@chingari.io>
2022-06-13 15:20:22 +0530

    Room model

--- base/views.py
+++ base/views.py
@@ -1,23 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Room
+
 # Create your views here.
 
-rooms = [
-    { 'id': 1, 'name': 'Room 1', 'description': 'This is room 1' },
-    { 'id': 2, 'name': 'Room 2', 'description': 'This is room 2' },
-    { 'id': 3, 'name': 'Room 3', 'description': 'This is room 3' },
-]
-
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
-            break
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
