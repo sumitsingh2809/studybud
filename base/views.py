205e1d8e294428f9ca3f9818482138a68e8a0372
Sumit Singh <sumitsingh@chingari.io>
2022-06-13 11:18:40 +0530

    templates

--- base/views.py
+++ base/views.py
@@ -3,8 +3,21 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home(request):
-    return render(request, 'home.html')
+rooms = [
+    { 'id': 1, 'name': 'Room 1', 'description': 'This is room 1' },
+    { 'id': 2, 'name': 'Room 2', 'description': 'This is room 2' },
+    { 'id': 3, 'name': 'Room 3', 'description': 'This is room 3' },
+]
 
-def room(request):
-    return render(request, 'room.html')
+def home(request):
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
+
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+            break
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
